a6c3df524ce78062f8180c2f0330740c43e54dc1
lukkan78 <lukkan@gmail.com>
2025-05-27 19:07:05 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,25 +1,23 @@
 
 const CACHE_NAME = 'pef-logg-cache-v1';
 const urlsToCache = [
-  '/',
-  '/pef_logg.html',
-  '/style.css',
-  '/app.js',
-  '/manifest.json'
+  './',
+  './index.html',
+  './style.css',
+  './app.js',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
